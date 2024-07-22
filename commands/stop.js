4ee0fb8a75a02b939a650866d36c487b05ead504
MysteriousGrimReaper <hitherehangouts@gmail.com>
2024-07-21 19:44:47 -0700

    bloat removed

--- commands/stop.js
+++ commands/stop.js
@@ -4,15 +4,9 @@ const db = new QuickDB();
 const games = db.table("games");
 module.exports = {
 	name: `stop`,
-	aliases: [`x`, `exit`],
+	aliases: [`x`, `exit`, `pause`],
 	description: `Pause a game occurring in the channel. The game will still be ongoing, but players cannot send inputs.`,
 	async execute(message) {
-		const { channel } = message;
-		if (!(await games.get(`${channel.id}.on`))) {
-			await channel.send(`No game is ongoing in this channel right now!`);
-			return;
-		}
-		await games.set(`${channel.id}.on`, false);
-		await channel.send(`UNO game paused.`);
+		// await channel.send(`UNO game paused.`);
 	},
 };
