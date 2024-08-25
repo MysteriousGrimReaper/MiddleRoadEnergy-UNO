d888f73e32926f5350742f330cebe185f515d82a
MysteriousGrimReaper <hitherehangouts@gmail.com>
2024-08-25 14:46:53 -0700

    ping will no longer ping if it's the same turn

--- inputs/play.js
+++ inputs/play.js
@@ -559,7 +559,7 @@ module.exports = {
 				);
 			}
 			chain_update();
-			if (game.players[game.table.current_turn].ping) {
+			if (game.players[game.table.current_turn].ping && current_turn != game.table.current_turn) {
 				await channel.send(
 					`<@${game.players[game.table.current_turn].id}>`
 				);
